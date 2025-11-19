108e08b25350ed4c6565feb18d7ab08caf877750
Muhammad Hamad Khan <mhk61366@gmail.com>
2025-11-19 18:44:54 +0500

    Add files via upload

---
+++ Python_dictionaires.py
@@ -0,0 +1,11 @@
+d1 = {"apples":6,"oranges":4,"mangoes":8,"banana":7,"grapes":12}
+
+value = input("Enter a value to search: ")
+value = int(value)
+
+new_d1 = {key: val for key,val in d1.items() if val==value }
+if new_d1:
+        print("Matching Key values : ")
+        print(new_d1)
+else:
+    print("No match found for the value")
